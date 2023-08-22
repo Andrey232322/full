d033f2e00a3cdbb6d201b6224925d88567508961
Andrey <dwigatel3532@gmail.com>
2023-08-22 18:13:59 +0300

    add router

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-from fastapi import FastAPI,Depends
-from sqlalchemy.orm import Session
-import crud
-from models.db import get_db, Base, engine
-from models.shemas_db import User
+from fastapi import FastAPI
+from router import user_router
+from models.db import Base, engine
 
 
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
-@app.get('/home{id}')
-def show_user(id : int = None,db:Session = Depends(get_db)):
-    user = crud.get_user(id,db)
-    return user
-
-@app.post('/home/user')
-def create_user(data: User  = None, db:Session = Depends(get_db)):
-    user = crud.create_user(data,db)
-    return user
-@app.get('/user/all')
-def show_user(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud.show_user_all(db,skip=skip,limit=limit)
-
-@app.delete('/user/')
-def delete_user(id:int,db:Session = Depends(get_db)):
-    return crud.delete_user(db,id)
-
-@app.put('/user')
-def update_user(id:int, data:User= None, db:Session = Depends(get_db)):
-    return crud.update(data,db,id)
+app.include_router(user_router)
---
+++ router.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+import crud
+from models.shemas_db import User
+from models.db import get_db
+
+user_router = APIRouter(prefix='/users',tags=['user'])
+
+@user_router.get('/{id}')
+def show_user(id : int = None,db:Session = Depends(get_db)):
+    user = crud.get_user(id,db)
+    return user
+@user_router.get('/all')
+def show_users_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.show_user_all(db,skip=skip,limit=limit)
+@user_router.post('/')
+def create_user(data: User  = None, db:Session = Depends(get_db)):
+    user = crud.create_user(data,db)
+    return user
+
+@user_router.delete('/')
+def delete_user(id:int,db:Session = Depends(get_db)):
+    return crud.delete_user(db,id)
+
+@user_router.put('/')
+def update_user(id:int, data:User= None, db:Session = Depends(get_db)):
+    return crud.update(data,db,id)
