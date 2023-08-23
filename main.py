ad46ffe03964758479977993796993fe764d270a
Andrey <dwigatel3532@gmail.com>
2023-08-23 17:44:00 +0300

    !

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from router import user_router
-from models.db import Base, engine
-from models.db import get_db
+from models.db import Base, engine, get_db
+from models.shemas_db import User
 from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 import crud
--- models/model_db.py
+++ models/model_db.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 
 from sqlalchemy import Column,String,INTEGER,BOOLEAN,TIMESTAMP
 from models.db import Base
