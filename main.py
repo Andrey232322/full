597f5341860de110b486be497dd7714b1b0672a4
Andrey <dwigatel3532@gmail.com>
2023-08-26 17:39:59 +0300

    refaktor

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Depends
-from fastapi_users import fastapi_users, FastAPIUsers
-
+from fastapi_users import FastAPIUsers
 from auth.manager_auth import auth_backend
 from auth.database_auth import User, create_db_and_tables
 from auth.manager_auth import get_user_manager, current_active_user
-from auth.schemas_auth import UserRead, UserCreate, UserUpdate
+from auth.schemas_auth import UserRead, UserCreate
 from operation.router_oper import router_oper
-from router import user_router
+from models.router import user_router
 from models.db import Base, engine
 
 
@@ -19,12 +18,9 @@ fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
 )
-
+app.include_router(router_oper)
 app.include_router(user_router)
-
-
 ###
-
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
@@ -40,15 +36,10 @@ app.include_router(
     prefix="/auth",
     tags=["auth"],
 )
-
-app.include_router(router_oper)
-
-@app.get("/authenticated-route")
+@app.get("/authenticated-route",tags=['authenticated'])
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
 
-
 @app.on_event("startup")
 async def on_startup():
-    # Not needed if you setup a migration system like Alembic
     await create_db_and_tables()
--- models/router.py
+++ models/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
-import crud
+from models import crud
 from models.shemas_db import User
 from models.db import get_db
 
@@ -8,20 +8,20 @@ user_router = APIRouter(prefix='/users',tags=['user'])
 
 @user_router.get('/{id}')
 def show_user(id : int = None,db:Session = Depends(get_db)):
-    user = crud.get_user(id,db)
+    user = crud.get_user(id, db)
     return user
 @user_router.get('/')
 def show_users_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud.show_user_all(db,skip=skip,limit=limit)
+    return crud.show_user_all(db, skip=skip, limit=limit)
 @user_router.post('/')
 def create_user(data: User  = None, db:Session = Depends(get_db)):
-    user = crud.create_user(data,db)
+    user = crud.create_user(data, db)
     return user
 
 @user_router.delete('/')
 def delete_user(id:int,db:Session = Depends(get_db)):
-    return crud.delete_user(db,id)
+    return crud.delete_user(db, id)
 
 @user_router.put('/')
 def update_user(id:int, data:User= None, db:Session = Depends(get_db)):
-    return crud.update(data,db,id)
+    return crud.update(data, db, id)
