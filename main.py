745bcc6f344082385d2b2c6400dd2114e6c1eb27
Andrey <dwigatel3532@gmail.com>
2023-08-26 16:40:35 +0300

    :/

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ app.include_router(user_router)
 ###
 
 
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
 )
@@ -34,21 +33,13 @@ app.include_router(
     prefix="/auth",
     tags=["auth"],
 )
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
+
 app.include_router(
     fastapi_users.get_verify_router(UserRead),
     prefix="/auth",
     tags=["auth"],
 )
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
+
 
 
 @app.get("/authenticated-route")
